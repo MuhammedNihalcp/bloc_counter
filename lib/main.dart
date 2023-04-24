e9fc316fc92432dff3ab69bf86a3a22fcb63afb1
MuhammedNihalcp <nihalriju915@gmail.com>
2023-04-24 06:30:46 +0530

    update file name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
+
 import 'package:bloc_counter/screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'logic/counter_cubit/counter_cubit.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/screen.dart
+++ lib/screen.dart
@@ -1,6 +1,8 @@
-import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'logic/counter_cubit/counter_cubit.dart';
 
 class ScreenCounter extends StatelessWidget {
   const ScreenCounter({super.key});
